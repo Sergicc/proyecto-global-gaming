a5de51e9d05c0c03b57dece1634541ec163ab475
jbosch1996 <boschruzjoan@gmail.com>
2017-04-24 20:34:45 +0200

    Home Cambiada(en proceso)

--- src/main/java/com/globalgaming/ProyectoGlobalGamingApp.java
+++ src/main/java/com/globalgaming/ProyectoGlobalGamingApp.java
@@ -36,7 +36,7 @@ public class ProyectoGlobalGamingApp {
      * <p>
      * Spring profiles can be configured with a program arguments --spring.profiles.active=your-active-profile
      * <p>
-     * You can find more information on how profiles work with JHipster on <a href="http://jhipster.github.io/profiles/">http://jhipster.github.io/profiles/</a>.
+     * You can find more information on how profiles work with JHipster on <a href="http://jhipster.github.io-/profiles/">http://jhipster.github.io/profiles/</a>.
      */
     @PostConstruct
     public void initApplication() {
--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -9,7 +9,7 @@
 
     function HomeController ($scope, Principal, LoginService, $state) {
         var vm = this;
-
+    vm.control = "true";
         vm.account = null;
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
